05028f4782c10b4872aeb2088065e5c4379bde2b
工厂 <55011096+ange473@users.noreply.github.com>
2019-11-10 15:50:36 +0800

    第10周实验课作业

---
+++ src/第十周作业/Example15.java
@@ -0,0 +1,12 @@
+package 第十周作业;
+
+public class Example15 {
+	public static void main (String [] args) {
+		Storage st =new Storage ();
+		Input input =new Input (st);
+		Output output =new Output(st);
+		new Thread(input).start();
+		new Thread(output).start();
+	}
+	
+}
---
+++ src/第十周作业/Output.java
@@ -0,0 +1,14 @@
+package 第十周作业;
+
+class Output implements Runnable{
+	private Storage st;
+
+	Output (Storage st ) {
+		this.st =st ;
+	}
+	public void run () {
+		while (true ) {
+			st.logOut();
+		}
+	}
+}

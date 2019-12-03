dc1808813aee98d59759e2043f7cca7d0ff0ecb1
工厂 <55011096+ange473@users.noreply.github.com>
2019-12-03 21:02:01 +0800

    PBL

---
+++ PBL/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+		public static void main(String[] args) {
+			Customer person1=new Customer("HuangZhichao");
+			Movie movie1=new Movie("Harry Potter",23);
+			Rental rental1=new Rental(movie1,3);
+			person1.addRental(rental1);
+			System. out.print(person1. statement());
+		}
+}
